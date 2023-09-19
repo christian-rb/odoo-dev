147c62716bc81629d25d83a8b76e2e1d9eca79a5
Tom De Caluwé <tdc@odoo.com>
2023-09-19 17:03:41 +0200

    [FIX] sale: update price_unit before recomputing taxes
    Before this commit, recomputing the taxes on a sale order after updating the
    fiscal position will not update the price_unit set on the sale order lines. As
    a consequence, when taxes included in the price are mapped by the new fiscal
    position, this will change the price before taxes.
    
    In contrast, when adding the same product after updating the fiscal position,
    the price_unit does reflect the changed taxes and will adapt the price_unit to
    make sure the price before taxes stays the same.
    
    This makes sense: let's say the fiscal position applies a tax exemption for a
    selected partner: this should not change the price before taxes.
    
    This commit makes sure the computed prices are the same, regardless of when the
    product was added: before or after changing the fiscal position.
    
    opw-3335939
    
    Co-authored-by Andrea Grazioso <agr@odoo.com>

--- addons/sale/tests/test_sale_tax_totals.py
+++ addons/sale/tests/test_sale_tax_totals.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.account.tests.test_invoice_tax_totals import TestTaxTotals
+from odoo.fields import Command
 from odoo.tests import tagged
 
 
@@ -33,3 +34,74 @@ class SaleTestTaxTotals(TestTaxTotals):
             'partner_id': self.partner_a.id,
             'order_line': lines_vals,
         })
+
+    def test_action_recompute_taxes(self):
+        '''
+        This test verifies the taxes recomputation action that can be triggered
+        after updating the fiscal position on a sale order document.
+        '''
+        special_tax = self.env['account.tax'].create({
+            'name': "special_tax_10",
+            'amount_type': 'percent',
+            'amount': 25.0,
+            'tax_group_id': self.tax_group1.id,
+            'include_base_amount': True,
+            'price_include': True,
+        })
+
+        mapped_tax_a = self.env['account.tax'].create({
+            'name': "tax_a",
+            'amount_type': 'percent',
+            'amount': 12.5,
+            'tax_group_id': self.tax_group1.id,
+            'include_base_amount': True,
+            'price_include': True,
+        })
+
+        mapped_tax_b = self.env['account.tax'].create({
+            'name': "tax_b",
+            'amount_type': 'percent',
+            'amount': 5.0,
+            'tax_group_id': self.tax_group1.id,
+            'include_base_amount': True,
+            'price_include': True,
+        })
+
+        sales_tax = self.env['account.tax'].create({
+            'name': "VAT 20%",
+            'amount_type': 'percent',
+            'amount': 20.0,
+            'tax_group_id': self.tax_group1.id,
+            'price_include': True,
+        })
+
+        mapping_a = self.env['account.fiscal.position'].create({
+            'name': 'Special Tax Reduction',
+            'tax_ids': [Command.create({'tax_src_id': special_tax.id, 'tax_dest_id': mapped_tax_a.id})],
+        })
+        mapping_b = self.env['account.fiscal.position'].create({
+            'name': 'Special Tax Reduction',
+            'tax_ids': [Command.create({'tax_src_id': special_tax.id, 'tax_dest_id': mapped_tax_b.id})],
+        })
+
+        # taxes and standard price need to be set on the product, as they will be
+        # recomputed when changing the fiscal position.
+        self.so_product.write({
+            'lst_price': 300,
+            'taxes_id': [Command.set((special_tax + sales_tax).ids)],
+        })
+
+        document = self._create_document_for_tax_totals_test([
+            (300, special_tax + sales_tax),
+        ])
+
+        self.assertEqual(document.amount_total, 300)
+        self.assertEqual(document.amount_tax, 100)
+        document.fiscal_position_id = mapping_a
+        document.action_update_taxes()
+        self.assertEqual(document.amount_total, 270)
+        self.assertEqual(document.amount_tax, 70)
+        document.fiscal_position_id = mapping_b
+        document.action_update_taxes()
+        self.assertEqual(document.amount_total, 252)
+        self.assertEqual(document.amount_tax, 52)
