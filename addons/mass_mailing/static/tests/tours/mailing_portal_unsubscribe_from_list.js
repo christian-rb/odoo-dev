ece061985ec93bd4bed40420f3c502ae0bd261bd
David Beguin <dbe@odoo.com>
2023-12-06 15:41:51 +0100

    [IMP] mass_mailing: rework design
    Purpose
    =======
    
    Make the opt-out page easier to customize as well as clearer by default.
    
    Specifications
    ==============
    
    1) Do not show the "Your Logo" if none has been set
    2) Design and rewording
    - Remove title bottom border
    - The unsub form should have the same width as the small one in the options
    - Rewrite subscribed to On/off
    - Reword sub/unsub/blocklist messages to include email address or the name of
    the mailing list.
    3) When unsubscribing, the feedback form should appear first
    4) Display the website header to allow to easily jump out
    5) Just above the footer, display a social media section (from web editor)
    6) Change the unsub form to this (the font was the one from the theme)
    7) Make change immediate using toggle buttons to un/subscribe (On/Off)
    8) Don't ask for feedback when switching off a mailing list. Only ask feedback
    when unsubscribing from email.
    9) Use default False for the option enabling the 'Exclude Me' button on the
    unsubscribe page.
    10) Do not show subscriptions of private lists.
    11) Ensure placeholder when subscriptions are all private.
    
    Task-3546018

--- addons/mass_mailing/static/tests/tours/mailing_portal_unsubscribe_from_list.js
+++ addons/mass_mailing/static/tests/tours/mailing_portal_unsubscribe_from_list.js
@@ -11,7 +11,7 @@ registry.category("web_tour.tours").add('mailing_portal_unsubscribe_from_list', 
     steps: () => [
         {
             content: "Confirmation unsubscribe is done",
-            trigger: "div#o_mailing_subscription_info span:contains('You are no longer part of the List1, List2 mailing list')",
+            trigger: "div#o_mailing_subscription_info span:contains('You have successfully unsubscribed from List1, List2')",
         }, {
             content: "Feedback textarea not displayed (see data)",
             trigger: "div#o_mailing_portal_subscription:not(textarea)",
@@ -51,13 +51,13 @@ registry.category("web_tour.tours").add('mailing_portal_unsubscribe_from_list_wi
     steps: () => [
         {
             content: "Confirmation unsubscribe is done",
-            trigger: "div#o_mailing_subscription_info span:contains('You are no longer part of the List1, List2 mailing list')",
+            trigger: "div#o_mailing_subscription_info span:contains('You have successfully unsubscribed from List1, List2')",
         }, {
             content: "List1 is present, just opt-outed",
-            trigger: "ul#o_mailing_subscription_form_lists li.list-group-item:contains('List1') span:contains('Not subscribed')",
+            trigger: "ul#o_mailing_subscription_form_lists li.list-group-item:contains('List1') span:contains('Off')",
         }, {
             content: "List3 is present, opt-outed (test starting data)",
-            trigger: "ul#o_mailing_subscription_form_lists li.list-group-item:contains('List3') span:contains('Not subscribed')",
+            trigger: "ul#o_mailing_subscription_form_lists li.list-group-item:contains('List3') span:contains('Off')",
         }, {
             content: "List2 is proposed (not member -> proposal to join)",
             trigger: "ul#o_mailing_subscription_form_lists_additional li.list-group-item:contains('List2')",
@@ -87,8 +87,17 @@ registry.category("web_tour.tours").add('mailing_portal_unsubscribe_from_list_wi
             content: "Confirmation exclusion is done",
             trigger: "div#o_mailing_subscription_update_info span:contains('Email added to our blocklist')",
         }, {
-            content: "This should disable the 'Update my subscriptions' (Apply changes) button",
-            trigger: "div#o_mailing_subscription_blocklist:not(button#button_form_send)",
+            content: "This should hide the subscription management panel",
+            trigger: "body",
+            run: function () {
+                const elements = this.anchor.querySelectorAll('div#o_mailing_subscription_form_manage:has(.d-none)');
+                if (elements.length === 1) {
+                    this.anchor.classList.add('manage_panel_hidden');
+                }
+            },
+        }, {
+            content: "Verify previous step",
+            trigger: "body.manage_panel_hidden",
             isCheck: true,
         }, {
             content: "Revert exclusion list",
@@ -97,27 +106,29 @@ registry.category("web_tour.tours").add('mailing_portal_unsubscribe_from_list_wi
             content: "Confirmation exclusion list is removed",
             trigger: "div#o_mailing_subscription_update_info span:contains('Email removed from our blocklist')",
         },  {
-            content: "'Update my subscriptions' button usable again",
-            trigger: "button#button_form_send:not([disabled])",
+            content: "Subscription panel displayed again and checkboxes available",
+            trigger: "ul#o_mailing_subscription_form_lists_additional",
             isCheck: true,
         }, {
             content: "Choose the mailing list 3 to come back",
             trigger: "ul#o_mailing_subscription_form_lists input[title='List3']",
+            run: "click",
+        }, {
+            content: "List 3 is noted as subscribed again",
+            trigger: "ul#o_mailing_subscription_form_lists li.list-group-item:contains('List3') span:contains('On')",
+            isCheck: true,
         }, {
             content: "Add list 2",
             trigger: "ul#o_mailing_subscription_form_lists_additional input[title='List2']",
-        }, {
-            content: "Update subscription",
-            trigger: "button#button_form_send",
-        }, {
-            content: "Confirmation changes are done",
-            trigger: "div#o_mailing_subscription_update_info span:contains('Membership updated')",
-        }, {
-            content: "List 3 is noted as subscribed again",
-            trigger: "ul#o_mailing_subscription_form_lists li.list-group-item:contains('List3') span:contains('Subscribed')",
+            run: "click",
         }, {
             content: "List 2 has joined the subscriptions",
-            trigger: "ul#o_mailing_subscription_form_lists li.list-group-item:contains('List2') span:contains('Subscribed')",
+            trigger: "ul#o_mailing_subscription_form_lists li.list-group-item:contains('List2') span:contains('On')",
+            isCheck: true,
+        }, {
+            content: "List 3 is still noted as subscribed",
+            trigger: "ul#o_mailing_subscription_form_lists li.list-group-item:contains('List3') span:contains('On')",
+            isCheck: true,
         }, {
             content: "No list in proposals",
             trigger: "div#o_mailing_subscription_form_manage:not(:has(ul#o_mailing_subscription_form_lists_additional))",
@@ -138,6 +149,7 @@ registry.category("web_tour.tours").add('mailing_portal_unsubscribe_from_list_wi
             content: "Warning should contain reference to memberships",
             trigger: "div#o_mailing_subscription_form_blocklisted li strong:contains('List2')",
             extra_trigger: "div#o_mailing_subscription_form_blocklisted li strong:contains('List3')",
+            isCheck: true,
         }, {
             content: "Give a reason for blocklist (first one)",
             trigger: "fieldset input.o_mailing_subscription_opt_out_reason:first",
