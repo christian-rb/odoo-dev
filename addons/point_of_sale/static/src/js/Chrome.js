ed2c6c66dbe73d04ef22a8316f23fd1642a0c2d4
Pierre Paridans <app@odoo.com>
2024-01-06 13:00:33 +0100

    fix! mounted

--- addons/point_of_sale/static/src/js/Chrome.js
+++ addons/point_of_sale/static/src/js/Chrome.js
@@ -106,7 +106,9 @@ odoo.define('point_of_sale.Chrome', function(require) {
                 console.error(error.cause);
             });
 
-            this.props.setupIsDone(this);
+            onMounted(() => {
+                this.props.setupIsDone(this);
+            });
         }
 
         // GETTERS //
