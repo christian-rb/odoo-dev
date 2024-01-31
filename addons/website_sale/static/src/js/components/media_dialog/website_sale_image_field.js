1981c0950986a214996c86e9571ab596e2227025
Soumya M <somu@odoo.com>
2024-02-01 01:28:58 +0530

    [IMP] website_sale: added support for multiple image uploads
    Before this commit:
    - Uploading multiple images on a product from backend was tricky as it
    only allowed single file upload.
    - There are no quick options on the media attached for edit or delete.
    
    After this commit:
    - Old form view for uploading images got replaced by MediaDialog, which
    supports multiple image uploads, and has a cleaner view for image and video
    upload screens.
    - Addition of Edit and Remove button over the image, for quick re-opening
    of MediaDialog to replace or remove the item respectively.
    
    Task-3390792

---
+++ addons/website_sale/static/src/js/components/media_dialog/website_sale_image_field.js
@@ -0,0 +1,71 @@
+import { ImageField, imageField } from "@web/views/fields/image/image_field";
+import { MultiMediaDialog } from "./website_sale_media_dialog";
+import { getVideoUrl } from "./website_sale_one2many";
+import { useService } from "@web/core/utils/hooks";
+import { registry } from "@web/core/registry";
+
+export class MultiMediaImageField extends ImageField {
+    static template = "website_sale.ImageField";
+    setup() {
+        super.setup();
+        this.orm = useService("orm");
+        this.dialog = useService("dialog");
+    }
+
+    /**
+     * New method and a new edit button is introduced here to overwrite,
+     * standard behavior of opening file input box in order to update a record.
+     */
+
+    onFileEdit(ev) {
+        const isVideo = this.props.record.data.video_url;
+        let mediaEl;
+        if (isVideo) {
+            mediaEl = document.createElement("img");
+            mediaEl.dataset.src = this.props.record.data.video_url;
+        }
+        this.dialog.add(MultiMediaDialog, {
+            onlyImages: true,
+            media: mediaEl,
+            activeTab: isVideo ? "VIDEOS" : "IMAGES",
+            save: (el) => {}, // Simple rebound to fake its execution
+            customImageSave: this.onCustomImageSave.bind(this),
+            customVideoSave: this.onCustomVideoSave.bind(this),
+        });
+    }
+
+    async onCustomImageSave(attachment) {
+        const attachmentRecord = await this.orm.searchRead(
+            "ir.attachment",
+            [["id", "=", attachment[0].id]],
+            ["id", "datas", "name"],
+            {}
+        );
+        await this.props.record.update({
+            [this.props.name]: attachmentRecord[0].datas,
+            name: attachmentRecord[0].name,
+        });
+    }
+
+    async onCustomVideoSave(videoInfo) {
+        const url = getVideoUrl(videoInfo[0].platform, videoInfo[0].videoId, videoInfo[0].params);
+        await this.props.record.update({
+            video_url: url.href,
+            name: videoInfo[0].platform + " - [Video]",
+        });
+    }
+
+    onFileRemove() {
+        const parentRecord = this.props.record?._parentRecord.data;
+        parentRecord?.product_template_image_ids
+            ? parentRecord.product_template_image_ids.delete(this.props.record)
+            : parentRecord.product_variant_image_ids.delete(this.props.record);
+    }
+}
+
+export const multiMediaImageField = {
+    ...imageField,
+    component: MultiMediaImageField,
+};
+
+registry.category("fields").add("multi_media_image", multiMediaImageField);
