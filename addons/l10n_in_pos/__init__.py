426f95ae3dd6d909ef1de626c31016756fbba365
Zeel Patel <zepa@odoo.com>
2024-02-22 14:41:58 +0530

    [IMP] l10n_in: set Default value for Indian company
    for Indian company, `Group by Product at Closing Entry` is True by Default.

--- addons/l10n_in_pos/__init__.py
+++ addons/l10n_in_pos/__init__.py
@@ -2,3 +2,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import models
+
+def pos_config_setting(env):
+    for config in env['pos.config'].search([('company_id.account_fiscal_country_id.code', '=', 'IN')]):
+        config.write({
+            'is_closing_entry_by_product': True
+        })
+
+def post_init(env):
+    pos_config_setting(env)
--- addons/l10n_in_pos/models/pos_config.py
+++ addons/l10n_in_pos/models/pos_config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models
-from odoo.exceptions import RedirectWarning
+from odoo import api, models
+from odoo.exceptions import RedirectWarning, UserError
 from odoo.tools.translate import _
 
 
@@ -22,3 +22,20 @@ class PosConfig(models.Model):
                 }
                 raise RedirectWarning(msg, action, _('Go to Company configuration'))
         return super(PosConfig, self).open_ui()
+
+    @api.constrains('is_closing_entry_by_product')
+    def _check_journal_entry_with_product_line(self):
+        for config in self:
+            if config.company_id.country_code == 'IN' and not config.is_closing_entry_by_product:
+                raise UserError("You can't deactivate the 'Closing Entry by product' option for Indian Companies.")
+
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in list(vals_list):
+            if self.env.company.country_code == 'IN':
+                vals.update({
+                    'is_closing_entry_by_product': True
+                })
+        pos_configs = super().create(vals_list)
+        return pos_configs
