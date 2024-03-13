ad40ce3a960f4a611b487de0219d6d56b21c5538
Maryam Kia <maki@odoo.com>
2024-03-13 15:02:59 +0100

    [FIX] mail, *: wrong order and content in OdooBot messages
    *: mail_bot, im_livechat
    
    Before:
    - In sending a message to odooBot, depending on how fast the
    bus notification is received, it could happen that the user message has still
    `temporary id` while the response from odooBot is already in
    `_handleNotificationNewMessage`. In this case the first message that is going to
     be inserted in channel is the one related to odooBot. So, the messages order
     is not correct.
     - The `odoobot_state` is set to disabled for admin user by default. This
     setting runs the chat with odoobot from the wrong state so although the bot
     state is not ready to start the tour, admin user can receive a message from bot
     containing `start the tour`.
    
    After:
     - The tmpMessage is only replaced by its genuine message not the first message
     which is received from the bus.
     - The default state (disabled) is deleted for admin in demo data . The first
     step for admin is same as other users. Also starting the tour again is possible
      after completing the tour.
    
    P.S.
    The part of fix related to crash on canned responses is done in odoo/odoo#162155
     and odoo/odoo#162234 (from V15.0 up to master)
    
    task-3570249

--- addons/im_livechat_mail_bot/models/mail_bot.py
+++ addons/im_livechat_mail_bot/models/mail_bot.py
@@ -13,11 +13,11 @@ class MailBot(models.AbstractModel):
             if odoobot_state == "onboarding_attachement" and values.get("attachment_ids"):
                 self.env.user.odoobot_failed = False
                 self.env.user.odoobot_state = "onboarding_canned"
-                return Markup(_("That's me! 🎉<br/>Try typing %s to use canned responses.", "<span class=\"o_odoobot_command\">:</span>"))
+                return Markup(_("Wonderful! 😇<br/>Try typing %s to use canned responses.", "<span class=\"o_odoobot_command\">:</span>"))
             elif odoobot_state == "onboarding_canned" and self.env.context.get("canned_response_ids"):
                 self.env.user.odoobot_failed = False
                 self.env.user.odoobot_state = "idle"
-                return Markup(_("Good, you can customize canned responses in the live chat application.<br/><br/><b>It's the end of this overview</b>, enjoy discovering Odoo!"))
+                return Markup(_("Good, you can customize canned responses in the discuss application.<br/><br/><b>It's the end of this overview</b>, enjoy discovering Odoo!"))
             # repeat question if needed
             elif odoobot_state == 'onboarding_canned' and not self._is_help_requested(body):
                 self.env.user.odoobot_failed = True
