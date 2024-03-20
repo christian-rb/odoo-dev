f0856669aba7225b5fa0e8d2fb36e44f35848906
Flotchet <mafl@odoo.com>
2024-03-20 12:46:14 +0000

    [FIX] hr_contract: take into account activeness
    Issue:
    When archiving a contract the employee form view does not update the
    first contract.
    
    Cause:
    The first contract computation is not trigger when archiving a contract.
    
    expected behavior:
    When archiving a contract the first contract should be recomputed.
    
    Fix:
    Add the active field to the depends method of the contract.
    
    task-3745594
    
    X-original-commit: 837a9c38bb7ca00d098259e7fbc492339951230f

--- addons/hr_contract/tests/__init__.py
+++ addons/hr_contract/tests/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_access
@@ -7,4 +6,9 @@ from . import test_contract
 from . import test_calendar_sync
 from . import test_mail_activity_plan
 from . import test_resource
+<<<<<<< HEAD
 from . import test_hr_contract_history_groupby
+||||||| parent of 5b35c0083f53 (temp)
+=======
+from . import test_employee
+>>>>>>> 5b35c0083f53 (temp)
