a93bd3774e25f18a2c13880b16933c7cc50e4ce2
Maxime de Neuville <mane@odoo.com>
2024-03-20 16:45:02 +0100

    [IMP] project,hr_timesheet,sale_timesheet: hide fields/filters/aggregations from project/task views
    After this commit, time fields (e.g. allocated_hours, effective_hours, ...)
    related to projects and tasks in the Project module are only visible if the user
    has the "hr_timesheet.group_hr_timesheet_user" group.
    Moreover, the corresponding filters and aggregations are only visible in such case.
    
    task-3815771

--- addons/hr_timesheet/report/project_report.py
+++ addons/hr_timesheet/report/project_report.py
@@ -7,14 +7,14 @@ from odoo import fields, models, api
 class ReportProjectTaskUser(models.Model):
     _inherit = "report.project.task.user"
 
-    allocated_hours = fields.Float('Allocated Time', readonly=True)
-    effective_hours = fields.Float('Hours Spent', readonly=True)
-    remaining_hours = fields.Float('Remaining Hours', readonly=True)
-    remaining_hours_percentage = fields.Float('Remaining Hours Percentage', readonly=True)
-    progress = fields.Float('Progress', aggregator='avg', readonly=True)
-    overtime = fields.Float(readonly=True)
-    total_hours_spent = fields.Float("Total Hours Spent", help="Time spent on this task, including its sub-tasks.")
-    subtask_effective_hours = fields.Float("Hours Spent on Sub-Tasks", help="Time spent on the sub-tasks (and their own sub-tasks) of this task.")
+    allocated_hours = fields.Float('Allocated Time', readonly=True, groups="hr_timesheet.group_hr_timesheet_user")
+    effective_hours = fields.Float('Hours Spent', readonly=True, groups="hr_timesheet.group_hr_timesheet_user")
+    remaining_hours = fields.Float('Remaining Hours', readonly=True, groups="hr_timesheet.group_hr_timesheet_user")
+    remaining_hours_percentage = fields.Float('Remaining Hours Percentage', readonly=True, groups="hr_timesheet.group_hr_timesheet_user")
+    progress = fields.Float('Progress', aggregator='avg', readonly=True, groups="hr_timesheet.group_hr_timesheet_user")
+    overtime = fields.Float(readonly=True, groups="hr_timesheet.group_hr_timesheet_user")
+    total_hours_spent = fields.Float("Total Hours Spent", help="Time spent on this task, including its sub-tasks.", groups="hr_timesheet.group_hr_timesheet_user")
+    subtask_effective_hours = fields.Float("Hours Spent on Sub-Tasks", help="Time spent on the sub-tasks (and their own sub-tasks) of this task.", groups="hr_timesheet.group_hr_timesheet_user")
 
     def _select(self):
         return super()._select() +  """,
