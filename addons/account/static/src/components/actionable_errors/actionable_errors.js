070e5bef31c6475bbbafd6e9000d988fbcd54dcd
Paolo Gatti (pgi) <pgi@odoo.com>
2024-03-26 10:37:08 +0100

    [IMP] account: ActionableErrors, add sorting
    SAF-T and other wizards relying on the `critical` attribute
    need sorting.
    jsonb fields don't retain order in PostgreSQL, so we re-sort
    the actionable_errors field by critical and then by error key.
    
    Enterprise PR: odoo/enterprise#59363
    task-3698183

--- addons/account/static/src/components/actionable_errors/actionable_errors.js
+++ addons/account/static/src/components/actionable_errors/actionable_errors.js
@@ -1,17 +1,46 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { Component } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
+import { Component } from "@odoo/owl";
 
 export class ActionableErrors extends Component {
-    static props = { ...standardFieldProps };
     static template = "account.ActionableErrors";
+    static props = { errorData: {type: Object} };
 
-    async handleOnClick(errorData){
-        this.env.model.action.doAction(errorData.action);
+    setup() {
+        super.setup();
+        this.actionService = useService("action");
+    }
+
+    async handleOnClick(clickedError) {
+        this.actionService.doAction(clickedError.action);
+    }
+
+    get errorData() {
+        return this.props.errorData;
+    }
+
+    get sortedActionableErrors() {
+        // With no fixed errorData set, then information is stored
+        // on a jsonb field in the DB. jsonb fields lose order
+        // so we sort it back by ("critical", key)
+        return Object.fromEntries(Object.entries(this.errorData).sort((a, b) =>
+              a[1]["critical"] && !b[1]["critical"] ? -1
+            : !a[1]["critical"] && b[1]["critical"] ? 1
+            : a[0].localeCompare(b[0])
+        ));
     }
 }
 
-export const actionableErrors = {component: ActionableErrors};
-registry.category("fields").add("actionable_errors", actionableErrors);
+export class ActionableErrorsField extends ActionableErrors {
+    static props = { ...standardFieldProps };
+
+    get errorData() {
+        return this.props.record.data[this.props.name];
+    }
+}
+
+export const actionableErrorsField = {component: ActionableErrorsField};
+registry.category("fields").add("actionable_errors", actionableErrorsField);
