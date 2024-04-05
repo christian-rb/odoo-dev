b2654d10be43e759384a53ba5aa6b4136616e29b
Maryam Kia <maki@odoo.com>
2024-04-05 10:52:03 +0200

    [REM] im_livechat_mail_bot, mail_bot: delete im_livechat_mail_bot module
    Since canned response feature is moved to mail module and im_livechat_mail_bot
    module only changes the odoo bot behavior to cover canned responses in chat with
     OdooBot, we can move this part to `mail_bot` and remove`im_livechat_mail_bot`.
    
    task-3848027
    
    See odoo/odoo#160666

--- addons/mail_bot/models/mail_bot.py
+++ addons/mail_bot/models/mail_bot.py
@@ -49,10 +49,19 @@ class MailBot(models.AbstractModel):
                 self.env.user.odoobot_state = "onboarding_attachement"
                 self.env.user.odoobot_failed = False
                 return Markup(_("Yep, I am here! 🎉 <br/>Now, try <b>sending an attachment</b>, like a picture of your cute dog..."))
-            elif odoobot_state == 'onboarding_attachement' and values.get("attachment_ids"):
-                self.env.user.odoobot_state = "idle"
+            elif odoobot_state == "onboarding_attachement" and values.get("attachment_ids"):
                 self.env.user.odoobot_failed = False
-                return Markup(_("I am a simple bot, but if that's a dog, he is the cutest 😊 <br/>Congratulations, you finished this tour. You can now <b>close this chat window</b>. Enjoy discovering Odoo."))
+                self.env.user.odoobot_state = "onboarding_canned"
+                return Markup(_("Wonderful! 😇<br/>Try typing %s to use canned responses.", "<span class=\"o_odoobot_command\">:</span>"))
+            elif odoobot_state == "onboarding_canned" and self.env.context.get("canned_response_ids"):
+                self.env.user.odoobot_failed = False
+                self.env.user.odoobot_state = "idle"
+                return Markup(_("Good, you can customize canned responses in discuss or live chat application.<br/><br/><b>It's the end of this overview</b>, enjoy discovering Odoo!"))
+            # repeat question if needed
+            elif odoobot_state == 'onboarding_canned' and not self._is_help_requested(body):
+                self.env.user.odoobot_failed = True
+                return _("Not sure what you are doing. Please, type %s and wait for the propositions. Select one of them and press enter.",
+                    Markup("<span class=\"o_odoobot_command\">:</span>"))
             elif odoobot_state in (False, "idle", "not_initialized") and (_('start the tour') in body.lower()):
                 self.env.user.odoobot_state = "onboarding_emoji"
                 return _("To start, try to send me an emoji :)")
