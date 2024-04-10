5b9ab9807302468f022e4f3b77d6cedb9cd2c168
paru-odoo <paru@odoo.com>
2024-04-10 16:52:25 +0530

    [FIX] website: make facebook snippet dynamically responsive
    Step to reproduce
    1. Drop inner content facebook (dynamic content) anywhere
    2. Go to mobile view
    3. Try to scroll horizontally.
    As you can see, facebook iframe is overflowing
    
    Before v16, the mechanism for mobile view was different. The switch
    between mobile and desktop views caused snippets/pages to be rerendered
    because a new dialog for the mobile was opened every time. Starting from
    version 16, we have removed the dialog and introduced a mobile frame.
    As a result, the rendering now occurs only when the start function is
    called. Consequently, the params width does not change when we switch to
    mobile view.
    
    After this commit, the snippet will be rerender every time when the
    window resizes, ensuring that the width adjustment is responsive. This
    enhancement also allows parameters to be provided at the time of
    rendering.
    
    Task-2736174

--- addons/website/static/src/snippets/s_facebook_page/000.js
+++ addons/website/static/src/snippets/s_facebook_page/000.js
@@ -3,6 +3,7 @@ odoo.define('website.s_facebook_page', function (require) {
 
 var publicWidget = require('web.public.widget');
 var utils = require('web.utils');
+const { debounce } = require("@web/core/utils/timing");
 
 const FacebookPageWidget = publicWidget.Widget.extend({
     selector: '.o_facebook_page',
@@ -12,37 +13,12 @@ const FacebookPageWidget = publicWidget.Widget.extend({
      * @override
      */
     start: function () {
-        var def = this._super.apply(this, arguments);
+        this._renderIframe();
 
-        this.options.wysiwyg && this.options.wysiwyg.odooEditor.observerUnactive();
+        this.resizeObserver = new ResizeObserver(debounce(this._renderIframe.bind(this), 100));
+        this.resizeObserver.observe(document.querySelector("#wrapwrap"));
 
-        const params = _.pick(this.$el[0].dataset, 'href', 'id', 'height', 'tabs', 'small_header', 'hide_cover');
-        if (!params.href) {
-            return def;
-        }
-        if (params.id) {
-            params.href = `https://www.facebook.com/${params.id}`;
-        }
-        delete params.id;
-        params.width = utils.confine(Math.floor(this.$el.width()), 180, 500);
-
-        var src = $.param.querystring('https://www.facebook.com/plugins/page.php', params);
-        this.$iframe = $('<iframe/>', {
-            src: src,
-            width: params.width,
-            height: params.height,
-            css: {
-                border: 'none',
-                overflow: 'hidden',
-            },
-            scrolling: 'no',
-            frameborder: '0',
-            allowTransparency: 'true',
-        });
-        this.$el.append(this.$iframe);
-
-        this.options.wysiwyg && this.options.wysiwyg.odooEditor.observerActive();
-        return def;
+        return this._super.apply(this, arguments);
     },
     /**
      * @override
@@ -54,6 +30,42 @@ const FacebookPageWidget = publicWidget.Widget.extend({
         if (this.$iframe) {
             this.$iframe.remove();
         }
+        this.options.wysiwyg && this.options.wysiwyg.odooEditor.observerActive();
+        this.resizeObserver.disconnect();
+    },
+    /**
+     * _renderIframe: prepare iframe element & replace it with existing iframe
+     *
+     * @private
+    */
+    _renderIframe: function () {
+        this.options.wysiwyg && this.options.wysiwyg.odooEditor.observerUnactive();
+
+        const params = _.pick(this.$el[0].dataset, 'href', 'id', 'height', 'tabs', 'small_header', 'hide_cover');
+        if (!params.href) {
+            return;
+        }
+        if (params.id) {
+            params.href = `https://www.facebook.com/${params.id}`;
+        }
+        delete params.id;
+        params.width = utils.confine(Math.floor(this.$el.width()), 180, 500);
+
+        var src = $.param.querystring('https://www.facebook.com/plugins/page.php', params);
+        const iframeEl = Object.assign(document.createElement("iframe"), {
+            src: src,
+            width: params.width,
+            height: params.height,
+            css: {
+                border: 'none',
+                overflow: 'hidden',
+            },
+            scrolling: 'no',
+            frameborder: '0',
+            allowTransparency: 'true',
+        });
+        this.el.replaceChildren(iframeEl);
+
         this.options.wysiwyg && this.options.wysiwyg.odooEditor.observerActive();
     },
 });
