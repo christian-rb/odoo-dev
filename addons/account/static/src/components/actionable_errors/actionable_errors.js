18802e8065e5308246f4a99221e0e16bb0a73518
aliya <alta@odoo.com>
2024-04-12 11:28:31 +0200

    [IMP] account: add level to actionable errors
    Currently, actionable errors are always a warning.
    This commit adds a possibility to adjust the level, so that we could show other types of alerts as well.
    
    Co-authored-by: lordkrandel <pgi@odoo.com>

--- addons/account/static/src/components/actionable_errors/actionable_errors.js
+++ addons/account/static/src/components/actionable_errors/actionable_errors.js
@@ -4,12 +4,25 @@ import { registry } from "@web/core/registry";
 import { Component } from "@odoo/owl";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
+const DISPLAY_ORDER = [
+    'danger',
+    'warning',
+    'info',
+];
+
 export class ActionableErrors extends Component {
     static props = { ...standardFieldProps };
     static template = "account.ActionableErrors";
 
     async handleOnClick(errorData){
         this.env.model.action.doAction(errorData.action);
+    }
+
+    get sortedActionableErrors() {
+        let data = this.props.record.data[this.props.name];
+        return Object.fromEntries(Object.entries(data).sort((a, b) => (
+            DISPLAY_ORDER.indexOf(a[1]["level"] || "warning") - DISPLAY_ORDER.indexOf(b[1]["level"] || "warning"))
+        ));
     }
 }
 
