29c6dbb64660c96d4c8fa124c5f43870aac501c7
Mohammad Abdulmoneim <abdu@odoo.com>
2024-04-16 17:11:58 +0200

    [FIX] sale_stock: display SN/lot in repair order invoices
    Bug:
    Invoices created for sales orders generated by a repair order didn't
    show SN/lot number for products that are tracked when activating
    "Display Lots & Serial Numbers on Invoices" setting. This was because
    the method generating the lots for the report only included stock move
    lines that had `customer` in its source or destination location usage,
    which is not the case in the move lines of a repair order.
    
    Fix:
    Exclude repair move lines from this check.
    
    Task-3848611

--- addons/sale_stock/models/account_move.py
+++ addons/sale_stock/models/account_move.py
@@ -62,7 +62,7 @@ class AccountMove(models.Model):
         previous_qties_delivered = defaultdict(float)
         stock_move_lines = current_invoice_amls.sale_line_ids.move_ids.move_line_ids.filtered(lambda sml: sml.state == 'done' and sml.lot_id).sorted(lambda sml: (sml.date, sml.id))
         for sml in stock_move_lines:
-            if sml.product_id not in invoiced_products or 'customer' not in {sml.location_id.usage, sml.location_dest_id.usage}:
+            if sml.product_id not in invoiced_products or (not sml.move_id.repair_line_type and 'customer' not in {sml.location_id.usage, sml.location_dest_id.usage}):
                 continue
             product = sml.product_id
             product_uom = product.uom_id
