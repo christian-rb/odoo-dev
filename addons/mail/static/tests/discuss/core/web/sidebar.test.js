7eec78bba86bcadfa36067772e094f1e6ddd3502
Julien Mougenot <jum@odoo.com>
2024-04-24 16:22:17 +0200

    [REF] *: remove rpcWithEnv

--- addons/mail/static/tests/discuss/core/web/sidebar.test.js
+++ addons/mail/static/tests/discuss/core/web/sidebar.test.js
@@ -1,4 +1,3 @@
-import { describe, test } from "@odoo/hoot";
 import {
     click,
     contains,
@@ -7,7 +6,8 @@ import {
     openDiscuss,
     start,
     startServer,
-} from "../../../mail_test_helpers";
+} from "@mail/../tests/mail_test_helpers";
+import { describe, test } from "@odoo/hoot";
 import { Command, serverState } from "@web/../tests/web_test_helpers";
 
 describe.current.tags("desktop");
