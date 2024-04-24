978f985062eb1b2b0567382327f8031ad162d1cb
Maximilien (malb) <malb@odoo.com>
2024-04-24 10:47:04 +0200

    [IMP] l10n_din5008: xml instead of python
    A lot of information of the din5008 layout were computed with some compute
    function which made the customization a bit hard to do.
    This commit will modify the l10n_din5008 module to pass all the logic in xml.
    
    Also updating the translation of the module.
    
    task-3888498

--- addons/l10n_din5008/models/base_document_layout.py
+++ addons/l10n_din5008/models/base_document_layout.py
@@ -1,8 +1,7 @@
 from markupsafe import Markup
 import re
 
-from odoo import api, models, fields, _
-from odoo.tools import format_date
+from odoo import api, models, fields
 
 
 class BaseDocumentLayout(models.TransientModel):
@@ -36,17 +35,3 @@ class BaseDocumentLayout(models.TransientModel):
     company_registry = fields.Char(related='company_id.company_registry', readonly=True)
     bank_ids = fields.One2many(related='company_id.partner_id.bank_ids', readonly=True)
     account_fiscal_country_id = fields.Many2one(related='company_id.account_fiscal_country_id', readonly=True)
-    l10n_din5008_template_data = fields.Binary(compute='_compute_l10n_din5008_template_data')
-    l10n_din5008_document_title = fields.Char(compute='_compute_l10n_din5008_document_title')
-
-    def _compute_l10n_din5008_template_data(self):
-        self.l10n_din5008_template_data = [
-            (_("Invoice No."), 'INV/2021/12345'),
-            (_("Invoice Date"), format_date(self.env, fields.Date.today())),
-            (_("Due Date"), format_date(self.env, fields.Date.add(fields.Date.today(), days=7))),
-            (_("Delivery Date"), format_date(self.env, fields.Date.add(fields.Date.today(), days=7))),
-            (_("Reference"), 'SO/2021/45678'),
-        ]
-
-    def _compute_l10n_din5008_document_title(self):
-        self.l10n_din5008_document_title = _('Invoice')
