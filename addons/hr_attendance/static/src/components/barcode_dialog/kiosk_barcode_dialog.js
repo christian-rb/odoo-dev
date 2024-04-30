2855624a9450bd80b055a39da5995dd64d7647f2
Mélanie <mepe@odoo.com>
2024-04-30 14:53:18 +0200

    e

--- addons/hr_attendance/static/src/components/barcode_dialog/kiosk_barcode_dialog.js
+++ addons/hr_attendance/static/src/components/barcode_dialog/kiosk_barcode_dialog.js
@@ -1,6 +1,22 @@
+import { onWillStart } from "@odoo/owl";
 import { BarcodeDialog } from "@web/webclient/barcode/barcode_scanner";
-
+import { useService } from "@web/core/utils/hooks";
 export class AttendanceBarcodeDialog extends BarcodeDialog {
+    static template = "hr_attendance.BarcodeDialog";
+
+    /**
+     * @override
+     */
+    setup(){
+        super.setup();
+        this.orm = useService("orm");
+
+        onWillStart( async () => {
+            this.employeeNumber = await this.orm.searchCount('hr.employee', []);
+            console.log(this.employeeNumber);
+        });
+    }
+
     /**
      * @override
      */
