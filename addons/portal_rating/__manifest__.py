keyboard-slayer/odoo-eval

[IMP] portal, mail, *: Use backend chatter in portal
By this commit:
- Portal chatter widget is deleted
- The chatter in portal is the backend Chatter component (from mail module) now
- Some features in chatter like link preview and reactions are new in portal
environment (number of queries in some tests are increased because of reactions
feature on messages)
- Except of two use cases of portal composer (as a popup composer) all the other
 use cases of composer in portal, are replaced by composer component in mail
 module
- The project sharing doesn't use the previous chatter component in the sharing
webclient view. It's replaced by backend chatter component as well

task-2828744

addons/portal_rating/__manifest__.py
@@ -22,13 +22,16 @@ inclusion of rating directly within the customer portal discuss widget.
     'assets': {
         'web.assets_frontend': [
             'portal_rating/static/src/scss/portal_rating.scss',
-            'portal_rating/static/src/js/portal_chatter.js',
             'portal_rating/static/src/xml/portal_chatter.xml',
             'portal_rating/static/src/js/portal_composer.js',
             'portal_rating/static/src/js/portal_rating_composer.js',
             'portal_rating/static/src/xml/portal_rating_composer.xml',
             'portal_rating/static/src/xml/portal_tools.xml',
+            'portal_rating/static/src/chatter/core/**/*',
         ],
+        'portal.assets_embed': [
+            'portal_rating/static/src/scss/portal_rating.scss',
+        ]
     },
     'license': 'LGPL-3',
 }

christian-rb/odoo-dev

addons/portal_rating/__manifest__.py
@@ -22,13 +22,16 @@ inclusion of rating directly within the customer portal discuss widget.
     'assets': {
         'web.assets_frontend': [
             'portal_rating/static/src/scss/portal_rating.scss',
-            'portal_rating/static/src/js/portal_chatter.js',
             'portal_rating/static/src/xml/portal_chatter.xml',
             'portal_rating/static/src/js/portal_composer.js',
             'portal_rating/static/src/js/portal_rating_composer.js',
             'portal_rating/static/src/xml/portal_rating_composer.xml',
             'portal_rating/static/src/xml/portal_tools.xml',
+            'portal_rating/static/src/chatter/frontend/**/*',
         ],
+        'portal.assets_embed': [
+            'portal_rating/static/src/scss/portal_rating.scss',
+        ]
     },
     'license': 'LGPL-3',
 }